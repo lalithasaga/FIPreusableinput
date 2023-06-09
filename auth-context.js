63469181a755217a4a91f009e523d0fa3ac09ded
lalithasaga <132065591+lalithasaga@users.noreply.github.com>
2023-06-09 22:34:07 +0530

    Add files via upload

---
+++ auth-context.js
@@ -0,0 +1,26 @@
+import React,{useState}from 'react';
+
+const AuthContext = React.createContext ({
+    isLoggedIn: false,
+    onLogout: (email,password) =>{}
+});
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn,setIsLoggedIn] = useState(false);
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler =() => {
+    setIsLoggedIn(true);
+    };
+    return <AuthContext.Provider 
+    value={{isLoggedIn: isLoggedIn,
+    onLogout:logoutHandler,
+    onLogin:loginHandler
+}}
+>
+    {props.children}
+    </AuthContext.Provider>;
+};
+export default AuthContext;
